736ca9479b9a0447f1119c21010b04bde709a12f
ritiksaxena124 <ritiksaxena124@gmail.com>
2024-07-04 13:51:37 +0530

    logic added to calculate age
    Signed-off-by: ritiksaxena124 <ritiksaxena124@gmail.com>

--- src/app/assignments/assignment-4/page.tsx
+++ src/app/assignments/assignment-4/page.tsx
@@ -9,12 +9,18 @@ interface DateTypes {
 }
 
 export default function Page() {
-  const [ageDate, setAgeDate] = useState<DateTypes>({
+  const [birthDate, setBirthDate] = useState<DateTypes>({
     day: "",
     month: "",
     year: "",
   });
   const [presentDate, setPresentDate] = useState<DateTypes>({
+    day: "",
+    month: "",
+    year: "",
+  });
+
+  const [userAge, setUsersAge] = useState<DateTypes>({
     day: "",
     month: "",
     year: "",
@@ -31,10 +37,62 @@ export default function Page() {
   }, []);
 
   function handleInputChange(field: string, e: SyntheticEvent) {
-    setAgeDate((prevData) => ({
+    setBirthDate((prevData) => ({
       ...prevData,
       [field]: (e.target as HTMLInputElement).value,
     }));
+  }
+
+  function isLeap(year: number): boolean {
+    if (year % 4 == 0) {
+      // checks if the century year is a leap year or not
+      if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  function getDaysInPrevMonth(currentMonth: number, currentYear: number) {
+    let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (isLeap(currentYear)) {
+      monthDays[1] = 29;
+    }
+
+    if (currentMonth - 1 == 0) return monthDays[monthDays.length - 1];
+    return monthDays[currentMonth - 1];
+  }
+
+  function usersAgeCalculate() {
+    let day;
+    let month;
+    let year;
+
+    year = Number(presentDate.year) - Number(birthDate.year);
+    if (presentDate.month > birthDate.month) {
+      month = Number(presentDate.month) - Number(birthDate.month);
+    } else {
+      month = Number(presentDate.month) - Number(birthDate.month) + 12;
+      year = year - 1;
+    }
+
+    if (presentDate.day > birthDate.day) {
+      day = Number(presentDate.day) - Number(birthDate.day);
+    } else {
+      day =
+        Number(presentDate.day) -
+        Number(birthDate.day) +
+        getDaysInPrevMonth(Number(presentDate.month), Number(presentDate.year));
+      month = month - 1;
+    }
+
+    setUsersAge({
+      day: JSON.stringify(day),
+      month: JSON.stringify(month),
+      year: JSON.stringify(year),
+    });
   }
 
   return (
@@ -42,39 +100,44 @@ export default function Page() {
       <div className="min-h-screen w-full flex items-center justify-center p-4 sm:px-8 bg-[#f0f0f0]">
         <div className="w-full md:max-w-3xl flex flex-col gap-6 p-8 sm:p-16 border shadow-xl bg-white rounded-l-[2rem] rounded-tr-[2rem] rounded-br-[7rem] sm:rounded-br-[10rem] ">
           <div className="flex gap-4 sm:gap-8 w-full sm:w-4/5">
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3 basis-1/3">
               <p className="uppercase font-medium text-zinc-600">day</p>
               <input
                 type="number"
                 className="outline outline-1 outline-zinc-400 rounded-md p-4  font-semibold text-2xl w-full focus:outline-zinc-800 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 autoFocus
                 onChange={(e) => handleInputChange("day", e)}
-                value={ageDate.day}
+                value={birthDate.day}
               />
             </div>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3 basis-1/3">
               <p className="uppercase font-medium text-zinc-600">month</p>
               <input
                 type="number"
                 className="outline outline-1 outline-zinc-400 rounded-md p-4  font-semibold text-2xl w-full focus:outline-zinc-800 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 onChange={(e) => handleInputChange("month", e)}
-                value={ageDate.month}
+                value={birthDate.month}
+                min={"1"}
+                max={"12"}
               />
             </div>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3 basis-1/3">
               <p className="uppercase font-medium text-zinc-600">year</p>
               <input
                 type="number"
                 className="outline outline-1 outline-zinc-400 rounded-md p-4  font-semibold text-2xl w-full focus:outline-zinc-800 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 onChange={(e) => handleInputChange("year", e)}
-                value={ageDate.year}
+                value={birthDate.year}
               />
             </div>
           </div>
 
           <div className="relative flex items-center justify-center h-24">
             <p className="h-[2px] w-full bg-zinc-300"></p>
-            <button className="w-16 h-16 md:w-24 md:h-24 bg-[#864CFF] absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 md:right-0 md:-translate-x-0 rounded-full flex items-center justify-center hover:bg-slate-800">
+            <button
+              onClick={usersAgeCalculate}
+              className="w-16 h-16 md:w-24 md:h-24 bg-[#864CFF] absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 md:right-0 md:-translate-x-0 rounded-full flex items-center justify-center hover:bg-slate-800"
+            >
               <img src={ArrowIcon?.src} alt="arrow-icon" />
             </button>
           </div>
@@ -82,25 +145,19 @@ export default function Page() {
           <div className="text-zinc-800">
             <h1 className="text-[3rem] sm:text-[5rem] md:text-[6rem] font-bold italic leading-none">
               <span className="text-[#864CFF]">
-                {ageDate.year == ""
-                  ? "-- "
-                  : Number(presentDate.year) - Number(ageDate.year) + " "}
+                {userAge.year == "" ? "-- " : userAge.year + " "}
               </span>
               years
             </h1>
             <h1 className="text-[3rem] sm:text-[5rem] md:text-[6rem] font-bold italic leading-tight">
               <span className="text-[#864CFF]">
-                {ageDate.month == ""
-                  ? "-- "
-                  : Number(presentDate.month) - Number(ageDate.month) + " "}
-              </span>{" "}
+                {userAge.month == "" ? "-- " : userAge.month + " "}
+              </span>
               months
             </h1>
             <h1 className="text-[3rem] sm:text-[5rem] md:text-[6rem] font-bold italic leading-tight">
               <span className="text-[#864CFF]">
-                {ageDate.day == ""
-                  ? "-- "
-                  : Number(presentDate.day) - Number(ageDate.day) + " "}
+                {userAge.day == "" ? "-- " : userAge.day + " "}
               </span>
               days
             </h1>
