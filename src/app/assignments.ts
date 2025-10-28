eb7b6c97b8a37ca40571429ade8be476ffcc08c6
ritiksaxena124 <ritiksaxena124@gmail.com>
2025-10-28 21:57:51 +0530

    fix: build issue

---
+++ src/app/assignments.ts
@@ -0,0 +1,42 @@
+export const assignments = [
+  {
+    id: 1,
+    title: "",
+  },
+  {
+    id: 2,
+    title: "Mortgage repayment calculator",
+  },
+  {
+    id: 3,
+    title: "Newsletter sign-up form",
+  },
+  {
+    id: 4,
+    title: "Age Calculator App",
+  },
+  {
+    id: 5,
+    title: "News Homepage",
+  },
+  {
+    id: 6,
+    title: "Notifications page",
+  },
+  {
+    id: 7,
+    title: "Interactive card details form",
+  },
+  {
+    id: 8,
+    title: "Expenses chart component",
+  },
+  {
+    id: 9,
+    title: "Intro section with dropdown navigation",
+  },
+  {
+    id: 10,
+    title: "Browser extension manager UI",
+  },
+];
