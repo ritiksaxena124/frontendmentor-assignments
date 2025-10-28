def0858dcf0291c208c60be006ce29790f7aae98
ritiksaxena124 <ritiksaxena124@gmail.com>
2025-10-28 23:13:20 +0530

    feat: assignment 10 initial setup

--- src/app/assignments/assignment-10/page.tsx
+++ src/app/assignments/assignment-10/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Header from "./_components/header";
 
 function Page() {
+  const filterButtons = [
+    {
+      label: "All",
+    },
+    {
+      label: "Active",
+    },
+    {
+      label: "Inactive",
+    },
+  ];
   return (
-    <div>page</div>
-  )
+    <div className="max-w-5xl mx-auto p-6 space-y-12">
+      <Header />
+      <main>
+        <div className="flex gap-4 justify-between items-center">
+          <h2 className="text-xl font-semibold">Extensions List</h2>
+          <span className="space-x-2">
+            {filterButtons.map((btn, index) => (
+              <Button
+                key={btn.label + index}
+                className="rounded-full"
+                variant={"outline"}
+              >
+                {btn.label}
+              </Button>
+            ))}
+          </span>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
